868f3e6842c2511d91027a0a9f872cf734384867
Philo Georgia Mary R <Philo.Georgia.Mary.R@gds.ey.com>
2022-11-26 12:43:59 +0530

    Initial Commit

---
+++ API/Program.cs
@@ -0,0 +1,20 @@
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(String[] args) {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(String[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
